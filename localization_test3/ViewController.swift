19ea26d789e19bbe5b9a310746c4f06f6278a8de
Luciano Lima <luciano.fernando@gmail.com>
2018-01-05 16:30:38 -0200

    Works

--- localization_test3/ViewController.swift
+++ localization_test3/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Atencao editar a view do (Main.storyboard/Base)
+    @IBOutlet weak var lblLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Metodo NSLocalizedString() encarrega de escolher a linguagem
+        lblLabel.text = NSLocalizedString("title_text", comment: "")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
